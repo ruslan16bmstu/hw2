c58ea2e663d35d4ea3d4f37d6cd2931581c58299
Ruslan <chugaev.r@mail.ru>
2021-11-08 16:13:43 +0300

    add mytests.cpp

---
+++ src/test/mytests.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+extern "C" {
+    #include "mylib.h"
+    #include "parallel.h"
+
+}
+
+TEST(parallel1, test1) {
+    char* name = "../text.txt";
+    char* str = readtext(name);
+    EXPECT_EQ(maxLen(str), 7);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+/*TEST(filesize, test) {
+    FILE *file;
+    file = fopen("../src/test/filesize_test0.txt", "r");
+    EXPECT_EQ(filesize(file), 0);
+    fclose(file);
+    file = fopen("../src/test/filesize_test1.txt", "r");
+    EXPECT_EQ(filesize(file), 17);
+    fclose(file);
+}
+
+TEST(isKey, test) {
+    char c = '\n';
+    char* str = "abcd\t\nabcd";
+    EXPECT_EQ(isKey(c, str), 1);
+    c = ' ';
+    EXPECT_EQ(isKey(c, str), 0);
+}*/
+
+/*TEST(parallel, test) {
+    //char* fname = "../src/test/maxLen_test1.txt";
+    //char* str = readtext(fname);
+    
+
+    char* name = "../text.txt";
+    char* str = readtext(name);
+    char* text = "abcd";
+    printf("%d\n", strlen(str));
+    printf("%d\n", strlen(text));
+    EXPECT_EQ(strcmp(text, str), 0);
+}*/
